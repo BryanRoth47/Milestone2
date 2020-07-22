d734a189996b726a400b56407cf0607cee1e0036
LordByron47 <55666548+LordByron47@users.noreply.github.com>
2020-07-22 12:00:17 -0700

    Bootstrap now works. Going to redo NavBar next.

--- src/components/navigationLinks.jsx
+++ src/components/navigationLinks.jsx
@@ -7,30 +7,36 @@ class NavigationLinks extends Component {
     render() {
 
         let dropDown;
+        let displayStyle = {
+            color: 'white', 
+        }
 
         if (this.props.section == 'navBar') {
             dropDown = <React.Fragment>
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ color: 'white', marginTop:'20px'}}>More...</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={displayStyle}>More...</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01" style={{background:'rgba(63,96,151,0.6)', minWidth:'100px'}} >
                     <LastTwoLinks section={this.props.section} />
                 </div>
             </React.Fragment>;
+
+            displayStyle.marginTop = '50px';
         }
         else {
             dropDown = <LastTwoLinks section={this.props.section} />;
+            displayStyle.marginTop = '10px';
         }
 
 
         return (
             <React.Fragment>
                 <a>
-                    <a class="py-2 d-none d-md-inline-block" href="#" style={{ color: 'white', marginTop:'20px'}}>Home</a><LineEnd section={this.props.section} />
+                    <a class="py-2 d-none d-md-inline-block" href="#" style={displayStyle}>Home</a><LineEnd section={this.props.section} />
                 </a>
 
-                <Link class="py-2 d-none d-md-inline-block" to="/software-engineering-internship" style={{ color: 'white', marginTop:'20px'}}>Software Engineering Virtual Internship</Link><LineEnd section={this.props.section} />
-                <a class="py-2 d-none d-md-inline-block" href="#test_link" target='_whatWeDo' style={{ color: 'white', marginTop:'20px'}}>Mission</a><LineEnd section={this.props.section} />
-                <a class="py-2 d-none d-md-inline-block" href="#" target='_whoWeAre' style={{ color: 'white', marginTop:'20px'}}>About</a><LineEnd section={this.props.section} />
-                <a class="py-2 d-none d-md-inline-block" href="#" target='_portfolio' style={{ color: 'white', marginTop:'20px'}}>Portfolio</a><LineEnd section={this.props.section} />
+                <Link class="py-2 d-none d-md-inline-block" to="/software-engineering-internship" style={displayStyle}>Software Engineering Virtual Internship</Link><LineEnd section={this.props.section} />
+                <a class="py-2 d-none d-md-inline-block" href="#test_link" target='_whatWeDo' style={displayStyle}>Mission</a><LineEnd section={this.props.section} />
+                <a class="py-2 d-none d-md-inline-block" href="#" target='_whoWeAre' style={displayStyle}>About</a><LineEnd section={this.props.section} />
+                <a class="py-2 d-none d-md-inline-block" href="#" target='_portfolio' style={displayStyle}>Portfolio</a><LineEnd section={this.props.section} />
 
                 {dropDown}
             </React.Fragment>
@@ -54,17 +60,24 @@ class LastTwoLinks extends Component {
 
     render() {
         let displayClass;
+        let displayStyle = {
+            color: 'white', 
+        }
+
         if (this.props.section === 'navBar') {
             displayClass = "dropdown-item";
         }
         else {
             displayClass = "py-2 d-none d-md-inline-block";
+            displayStyle.marginTop = '10px';
         }
+        
+        
 
         return (
             <React.Fragment>
-                <a class={displayClass} href="#" target='_internships' style={{ color: 'white', marginTop:'20px'}}>Internships</a><LineEnd section={this.props.section} />
-                <a class={displayClass} href="#" target='_contact' style={{ color: 'white', marginTop:'20px'}}>Contact</a>
+                <a class={displayClass} href="#" target='_internships' style={displayStyle}>Internships</a><LineEnd section={this.props.section} />
+                <a class={displayClass} href="#" target='_contact' style={displayStyle}>Contact</a>
             </React.Fragment>
         );
     }
