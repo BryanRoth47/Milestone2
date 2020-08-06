8ce6a08490f5ec8d9832a420baaa08fea77b1306
LordByron47 <55666548+LordByron47@users.noreply.github.com>
2020-08-05 17:04:20 -0700

    Improved styling

---
+++ src/components/SiteSections/HomePage/EmailForm.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+
+export class EmailForm extends Component {
+
+    /*
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: "mailto:baroth@uci.edu?subject=Interested Customer"
+        }
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+
+    
+    //mailto:info@ardentlabs.io?subject=Resume for Software Engineering Remote Internship"
+    handleSubmit(event) {
+        event.preventDefault();
+        const destination = "mailto:baroth@uci.edu?cc=";
+        let cc = document.getElementById("emailAddress");
+        let emailToSend = destination.concat(cc.value.toString());
+        emailToSend = emailToSend.concat("&subject=Interested Customer")
+        let body = document.getElementById("emailBody");
+        emailToSend = emailToSend.concat("&body=").concat(body.value);
+        console.log(emailToSend);
+        this.setState(
+            { url: emailToSend}, ()=>{
+                alert("callback: "+this.state.url);
+                const form = document.getElementById("emailForm");
+                form.submit();
+            });
+        
+    }
+    */
+    render() {
+
+        return (
+            <form id="emailForm">
+                <div className="form-group">
+                    <input id="emailAddress" type="email" className="form-control" required maxLength='250' style={{
+                        width: '80%', paddingLeft: '12px', borderWidth: '3px', borderStyle: 'solid',
+                        borderColor: 'rgba(100, 102, 102, 1)'
+                    }} placeholder="Email" />
+                    <textarea className="form-control" id="emailBody" rows='5' style={{ resize:"none",
+                        width: '80%', marginTop: '1em', paddingLeft: '16px', paddingRight: '10px',
+                        boxSizing: 'border-box', borderWidth: '3px', borderStyle: 'solid',
+                        borderColor: 'rgba(100, 102, 102, 1)'
+                    }} placeholder="Type your message here..." />
+                    <div style={{ width: '80%', display: 'flex', justifyContent: 'flex-end' }}>
+                        <input type="submit" className="btn" style={{ backgroundColor: 'transparent', borderStyle: 'none' }} />
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default EmailForm
--- src/components/SiteSections/HomePage/internships.jsx
+++ src/components/SiteSections/HomePage/internships.jsx
@@ -1,53 +1,41 @@
 import React, { Component } from 'react';
+import EmailForm from './EmailForm';
 
 class Internships extends Component {
 
     render() {
         return (
             <React.Fragment >
-                <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 ardentGrey" id="internships" style={{ backgroundColor: '#efefef', width: "105%", position: "relative", left: "-15px", top:"-48px" }}>
+                <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 ardentGrey" id="internships" style={{ backgroundColor: '#efefef', width: "105%", position: "relative", left: "-15px", top: "-48px" }}>
                     <div style={{ position: "relative", left: "200px" }}>
 
                         <h1 className="sectionHeading font88 ardentGrey" style={{ marginBottom: "20px", }}>INTERNSHIPS</h1>
 
                         <h3 className="font24">Software Engineer</h3>
-                        <p style={{ fontSize: "18px", width:'972px' }}>Learn the agile development process, master the latest front and back-end development tools, and
+                        <p style={{ fontSize: "18px", width: '972px' }}>Learn the agile development process, master the latest front and back-end development tools, and
                         take charge of an app from conception to production.</p>
 
                         <h3 className="font24">UI/UX/Product Manager</h3>
-                        <p style={{ fontSize: "18px", width:'972px' }}> Learn how to develop beautiful user interface and meaningful user experience and jump start your
+                        <p style={{ fontSize: "18px", width: '972px' }}> Learn how to develop beautiful user interface and meaningful user experience and jump start your
                         career in product management.</p>
 
                         <h3 className="font24">Digital Marketer</h3>
-                        <p style={{ fontSize: "18px", width:'972px' }}>Learn how to market software applications via online and offline marketing media with a special
+                        <p style={{ fontSize: "18px", width: '972px' }}>Learn how to market software applications via online and offline marketing media with a special
                         emphasis on social media marketing.</p><br />
-                        </div>
-                        <div className='container' id="contact">
-                            <div className='row'>
-                                <div className='col-md-4'>
-                                    <h6 className="font24 ardentBlue">Are you interested in developing a career and getting jobs at leading tech companies?
-                                    Send us a short message and we will contact you shortly.
+                    </div>
+                    <div className='container' id="contact">
+                        <div className='row'>
+                            <div className='col-md-4'>
+                                <h6 className="font24 ardentBlue">Are you interested in developing a career and getting jobs at leading tech companies?
+                                Send us a short message and we will contact you shortly.
                                 </h6>
-                                </div>
-                                <div className='col'>
-                                    <form>
-                                        <input type="email" required maxLength='250' style={{
-                                            width: '80%', paddingLeft: '12px', borderWidth: '3px', borderStyle: 'solid',
-                                            borderColor: 'rgba(100, 102, 102, 1)'
-                                        }} placeholder="Email" /><br />
-                                        <textarea rows='5' style={{
-                                            width: '80%', marginTop: '1em', paddingLeft: '16px', paddingRight: '10px',
-                                            boxSizing: 'border-box', borderWidth: '3px', borderStyle: 'solid',
-                                            borderColor: 'rgba(100, 102, 102, 1)'
-                                        }} placeholder="Type your message here..." /><br />
-                                        <div style={{ width: '80%', display: 'flex', justifyContent: 'flex-end' }}>
-                                            <input type="submit" style={{ backgroundColor: 'transparent', borderStyle: 'none' }} />
-                                        </div>
-                                    </form>
-                                </div>
+                            </div>
+                            <div className='col'>
+                                <EmailForm />
                             </div>
                         </div>
-                    
+                    </div>
+
                 </div>
             </React.Fragment>
         )
